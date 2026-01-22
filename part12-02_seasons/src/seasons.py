ba4bd0d2c1c8d12c9b91fb8e469c9d71a8f0e0e3
Naufal Ghifari A <nghifaria@gmail.com>
2026-01-22 13:12:45 +0700

    Add files via upload

---
+++ part12-02_seasons/src/seasons.py
@@ -0,0 +1,6 @@
+def sort_by_seasons(items: list):
+    def order_by_seasons(item: dict):
+        return item["seasons"]
+    return sorted(items, key=order_by_seasons)
+
+
---
+++ part12-03_ratings/src/ratings.py
@@ -0,0 +1,33 @@
+class Student:
+    """ The class models a single student"""
+    def __init__(self, name: str, id: str, credits: int):
+        self.name = name
+        self.id = id
+        self.credits = credits
+
+    def __str__(self):
+        return f"{self.name} ({self.id}), {self.credits} cr."
+    
+
+def by_id(item: Student):
+    return item.id
+
+def by_credits(item: Student):
+    return item.credits
+
+if __name__ == "__main__":
+    o1 = Student("Archie", "a123", 220)
+    o2 = Student("Marvin", "m321", 210)
+    o3 = Student("Anna", "a999", 131)
+
+    students = [o1, o2, o3]
+
+    print("Sort by id:")
+    for student in sorted(students, key= by_id):
+        print(student)
+
+    print()
+
+    print("Sort by credits:")
+    for student in sorted(students, key=by_credits):
+        print(student)
