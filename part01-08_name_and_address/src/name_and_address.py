99eeeffcc792b38cc10330636dc46b6a5f86ab3e
Naufal Ghifari A <nghifaria@gmail.com>
2026-03-09 10:31:30 +0700

    Add files via upload

---
+++ part01-08_name_and_address/src/name_and_address.py
@@ -0,0 +1,7 @@
+# Write your solution here
+given_name = input("Given name: ")
+family_name = input("Family name: ")
+str_address = input("Street address: ")
+city_and_postal_code = input("City and postal code: ")
+
+print(f"{given_name} {family_name}\n{str_address}\n{city_and_postal_code}")
