58ab8c4b1dfc82fe10d4033f3fb499e131d5c7ee
Naufal Ghifari A <nghifaria@gmail.com>
2026-03-12 05:45:28 +0700

    Add files via upload

---
+++ part01-10_story/src/story.py
@@ -0,0 +1,7 @@
+# Write your solution here
+ques = "Please type in a "
+
+name = input(ques + "name: ")
+year = input(ques + "year: ")
+
+print(f"{name} is a valiant knight, born in the year {year}. One morning {name} woke up to an awful racket: a dragon was approaching the village. Only {name} could save the village's residents.")
