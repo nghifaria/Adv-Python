82579128adcd1c1529a26e4e1143d0eaad02ee51
Naufal Ghifari A <nghifaria@gmail.com>
2026-03-25 00:34:55 +0700

    Add files via upload

---
+++ part01-23_soup_or_no_soup/src/soup_or_no_soup.py
@@ -0,0 +1,13 @@
+# Write your solution here
+
+name = input("Please tell me your name: ")
+
+
+if name != "Jerry":
+    soup = int(input("How many portions of soup? "))
+
+    total_cost = soup * 5.9
+
+    print(f"The total cost is {total_cost}")
+
+print("Next please!")
