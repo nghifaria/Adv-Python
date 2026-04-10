58a22e0e417d76155d7cca7988a8f9473eb4f2ff
Naufal Ghifari A <nghifaria@gmail.com>
2026-04-10 15:01:29 +0700

    Add files via upload

--- part02-09_nephews/src/nephews.py
+++ part02-09_nephews/src/nephews.py
@@ -1,1 +1,10 @@
 # Write your solution here
+donald_duck = ["Huey", "Dewey", "Louie"]
+mickey_mouse = ["Morty", "Ferdie"]
+name = input("Please type in your name: ")
+if name in donald_duck:
+    print("I think you might be one of Donald Duck's nephews.")
+elif name in mickey_mouse:
+    print("I think you might be one of Mickey Mouse's nephews.")
+else:
+    print("You're not a nephew of any character I know of.")
